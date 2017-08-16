745778c42e49b3e5ad3ef631b605c5ef82a784a0
Joost Vermeulen <joost@jover.net>
2017-08-16 15:00:00 +0200

    Ticket update hook
    Ticket update hook, which will sync the status if needed

--- src/Delegate.php
+++ src/Delegate.php
@@ -5,6 +5,7 @@ namespace AbuseIO\Hook;
 use AbuseIO\Jobs\FindContact;
 use AbuseIO\Hook\HookInterface;
 use AbuseIO\Models\Incident;
+use AbuseIO\Models\Ticket;
 use Zend\Http\Client;
 use Log as Logger;
 
@@ -39,24 +40,22 @@ class Delegate implements HookInterface
                 $url = $contact->api_host . "/incidents";
 
                 // send incident
-                Logger::notice('Sending incident to ' . $url);
-                $client = new Client($url);
-                $client->setHeaders([
-                    'Accept'      => 'application/json',
-                    'X-API-TOKEN' => $token
-                ]);
-                $client->setParameterPost($incident->toArray());
-                $client->setMethod('POST');
-                $response = $client->send();
+                self::send($url, $token, $incident->toArray());
+            }
+        }
 
-                if (!$response->isSuccess()) {
-                   Logger::notice(
-                       sprintf(
-                           "Failure, statuscode: %d\n body: %s\n",
-                           $response->getStatusCode(),
-                           $response->getBody()
-                       )
-                   );
+        if ($object instanceof $models['Ticket'] && $event == 'updating') {
+            // we are only interested in the statuses
+            $original = $object->getOriginal();
+            if (($object->status_id !== $original['status_id']) ||
+                ($object->contact_status_id !== $original['contact_status_id']))
+            {
+                // find the token and url
+                $api = self::getTicketUrl($object, '/syncstatus');
+                if (!empty($api)) {
+
+                    // send ticket
+                    self::send($api['url'], $api['token'], $object->toArray());
                 }
             }
         }
@@ -70,5 +69,61 @@ class Delegate implements HookInterface
     public static function isEnabled()
     {
         return true;
+    }
+
+    /**
+     * get the api_url and token from the ticket
+     *
+     * @param Ticket $ticket
+     * @param string $part
+     * @return array
+     */
+    private static function getTicketUrl(Ticket $ticket, $part = '')
+    {
+        $result = [];
+
+        if (!is_null($ticket->remote_api_url) && !is_null($ticket->remote_api_token)) {
+            $result['url'] = $ticket->remote_api_url . $part;
+            $result['token'] = $ticket->remote_api_token;
+        } else {
+            $contact = FindContact::byIP($ticket->ip);
+            if (!is_null($contact->api_host) && !is_null($contact->token)) {
+                $result['url'] = $contact->api_host . '/ticket' . $part;
+                $result['token'] = $contact->token;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * places the api call
+     *
+     * @param $url
+     * @param string $token
+     * @param array $data
+     */
+    private static function send($url, $token = '', $data = [])
+    {
+        // send incident
+        Logger::notice('Sending data to ' . $url);
+        $client = new Client($url);
+        $client->setHeaders([
+            'Accept'      => 'application/json',
+            'X-API-TOKEN' => $token
+        ]);
+        $client->setMethod('POST');
+        $client->setParameterPost($data);
+        $response = $client->send();
+
+        if (!$response->isSuccess()) {
+            Logger::notice(
+                sprintf(
+                    "Failure, statuscode: %d\n body: %s\n",
+                    $response->getStatusCode(),
+                    $response->getBody()
+                )
+            );
+        }
     }
 }
